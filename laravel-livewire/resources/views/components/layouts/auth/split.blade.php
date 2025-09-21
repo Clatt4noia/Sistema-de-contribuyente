e44dff377dac45012d368e09249ef9c24e11dcd1
DevBryan <briansoberonq@gmail.com>
2025-09-21 17:41:11 -0500

    login and register update

--- laravel-livewire/resources/views/components/layouts/auth/split.blade.php
+++ laravel-livewire/resources/views/components/layouts/auth/split.blade.php
@@ -8,10 +8,8 @@
             <div class="bg-muted relative hidden h-full flex-col p-10 text-white lg:flex dark:border-e dark:border-neutral-800">
                 <div class="absolute inset-0 bg-neutral-900"></div>
                 <a href="{{ route('home') }}" class="relative z-20 flex items-center text-lg font-medium" wire:navigate>
-                    <span class="flex h-10 w-10 items-center justify-center rounded-md">
-                        <x-app-logo-icon class="me-2 h-7 fill-current text-white" />
-                    </span>
-                    {{ config('app.name', 'Laravel') }}
+
+                    {{ config('app.name', 'bryanmenu') }}
                 </a>
 
                 @php
@@ -20,8 +18,8 @@
 
                 <div class="relative z-20 mt-auto">
                     <blockquote class="space-y-2">
-                        <flux:heading size="lg">&ldquo;{{ trim($message) }}&rdquo;</flux:heading>
-                        <footer><flux:heading>{{ trim($author) }}</flux:heading></footer>
+                        <flux:heading size="lg">&ldquo;{{ trim("SOPHISTICATED ENVIRONMENT") }}&rdquo;</flux:heading>
+                        <footer><flux:heading>{{ trim("Bryan Soberón") }}</flux:heading></footer>
                     </blockquote>
                 </div>
             </div>
