a7ccb16507b1af99d3226b0334d067c7d3d962e3
DevBryan <briansoberonq@gmail.com>
2025-09-22 23:51:15 -0500

    sidebar v1

--- laravel-livewire/resources/views/components/layouts/app/sidebar.blade.php
+++ laravel-livewire/resources/views/components/layouts/app/sidebar.blade.php
@@ -20,12 +20,12 @@
             <flux:spacer />
 
             <flux:navlist variant="outline">
-                <flux:navlist.item icon="folder-git-2" href="https://github.com/laravel/livewire-starter-kit" target="_blank">
-                {{ __('Repository') }}
+                <flux:navlist.item icon="folder-git-2" href="https://github.com/bryansoberon" target="_blank">
+                {{ __('Github bryan') }}
                 </flux:navlist.item>
 
-                <flux:navlist.item icon="book-open-text" href="https://laravel.com/docs/starter-kits#livewire" target="_blank">
-                {{ __('Documentation') }}
+                <flux:navlist.item icon="book-open-text" href="https://www.instagram.com/bryansoberon" target="_blank">
+                {{ __('Instagran') }}
                 </flux:navlist.item>
             </flux:navlist>
 
