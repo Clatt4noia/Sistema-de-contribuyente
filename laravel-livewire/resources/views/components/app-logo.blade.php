a47abcb0fea3c85d61e9575cf5d76e52b14d274f
DevBryan <briansoberonq@gmail.com>
2025-09-23 20:31:26 -0500

    logo pequeño

--- laravel-livewire/resources/views/components/app-logo.blade.php
+++ laravel-livewire/resources/views/components/app-logo.blade.php
@@ -2,5 +2,5 @@
     <x-app-logo-icon class="size-5 fill-current text-white dark:text-black" />
 </div>
 <div class="ms-1 grid flex-1 text-start text-sm">
-    <span class="mb-0.5 truncate leading-tight font-semibold">Bryan APP</span>
+    <span class="mb-0.5 truncate leading-tight font-semibold">CARLOS GABRIEL TRANSPORTES S.A.C.</span>
 </div>
