cc9c187ca343972a3d680453a59bc004a889de89
DevBryan <briansoberonq@gmail.com>
2025-09-23 20:41:37 -0500

    WORD C

--- laravel-livewire/resources/views/components/app-logo-icon.blade.php
+++ laravel-livewire/resources/views/components/app-logo-icon.blade.php
@@ -1,1 +1,7 @@
-<img src="{{ asset('logoPPP.png') }}" alt="Logo Transporte Juan Gabriel" width="40" height="42">
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 42">
+  <text x="50%" y="50%" text-anchor="middle" dominant-baseline="middle"
+        font-size="32" font-family="Arial, sans-serif"
+        fill="currentColor">
+    C
+  </text>
+</svg>
