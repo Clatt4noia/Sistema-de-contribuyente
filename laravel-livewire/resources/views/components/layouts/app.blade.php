75f78529f6b499bebf993d2d62530bbaeb3839c2
Bernabe Bryan Soberón Quintana <66850363+bryansoberon@users.noreply.github.com>
2025-09-28 13:07:04 -0500

    Adjust Flux layout wrappers

--- laravel-livewire/resources/views/components/layouts/app.blade.php
+++ laravel-livewire/resources/views/components/layouts/app.blade.php
@@ -1,3 +1,5 @@
 <x-layouts.app.sidebar :title="$title ?? null">
-    {{ $slot }}
+    <flux:main class="min-h-screen w-full px-4 py-6 sm:px-6 lg:px-8 mx-auto max-w-6xl space-y-6">
+        {{ $slot }}
+    </flux:main>
 </x-layouts.app.sidebar>
