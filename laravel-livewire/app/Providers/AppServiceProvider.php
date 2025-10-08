242fcd81068f0feae02435485ad13b720e9647c0
Bernabe Bryan Soberón Quintana <66850363+bryansoberon@users.noreply.github.com>
2025-10-07 23:51:10 -0500

    Documentar plan de integración SUNAT para facturación

--- laravel-livewire/app/Providers/AppServiceProvider.php
+++ laravel-livewire/app/Providers/AppServiceProvider.php
@@ -24,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
         // Registrar observer de órdenes
         \App\Models\Order::observe(\App\Observers\OrderObserver::class);
 
+<<<<<<< HEAD
         // ✅ Configurar localización en español (Perú)
         Carbon::setLocale('es');
         Date::setLocale('es');
@@ -36,5 +37,13 @@ class AppServiceProvider extends ServiceProvider
         config(['app.timezone' => 'America/Lima']);
         date_default_timezone_set('America/Lima');
 
+=======
+        Date::useLocale('es');
+        Carbon::setLocale('es');
+
+        if (function_exists('setlocale')) {
+            setlocale(LC_TIME, 'es_PE.UTF-8', 'es_PE', 'es');
+        }
+>>>>>>> 60f7131 (Documentar plan de integración SUNAT para facturación)
     }
 }
