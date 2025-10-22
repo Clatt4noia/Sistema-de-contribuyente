faac57ac2628e548472fddd7504de337532556a4
DevBryan <briansoberonq@gmail.com>
2025-10-21 20:59:56 -0500

    sin dark_v1

--- laravel-livewire/resources/views/components/theme/html.blade.php
+++ laravel-livewire/resources/views/components/theme/html.blade.php
@@ -1,27 +1,4 @@
-@props(['theme' => null])
-
-@php
-    $cookieTheme = request()->cookie('app_theme');
-
-    if (in_array($theme, ['light', 'dark'], true)) {
-        $resolvedTheme = $theme;
-    } elseif (in_array($cookieTheme, ['light', 'dark'], true)) {
-        $resolvedTheme = $cookieTheme;
-    } else {
-        $resolvedTheme = 'light';
-    }
-
-@endphp
-
 <!DOCTYPE html>
-<html {{ $attributes
-    ->merge([
-        'lang' => str_replace('_', '-', app()->getLocale()),
-        'data-theme' => $resolvedTheme,
-    ])
-    ->class([
-        $resolvedTheme === 'dark' ? 'dark' : null,
-    ])
-}}>
+<html {{ $attributes->merge(['lang' => str_replace('_', '-', app()->getLocale())]) }}>
     {{ $slot }}
 </html>
