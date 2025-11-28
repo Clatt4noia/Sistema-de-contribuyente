4f53cf515e23e67775ceea0351f1c9b678f2768e
Bernabe Bryan Soberón Quintana <66850363+bryansoberon@users.noreply.github.com>
2025-11-28 14:35:29 -0500

    Use logo image in sidebar icon

--- laravel-livewire/resources/views/components/app-logo-icon.blade.php
+++ laravel-livewire/resources/views/components/app-logo-icon.blade.php
@@ -1,7 +1,2 @@
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 42">
-  <text x="50%" y="50%" text-anchor="middle" dominant-baseline="middle"
-        font-size="32" font-family="Arial, sans-serif"
-        fill="currentColor">
-    C
-  </text>
-</svg>
+<img src="{{ asset('logoo1.png') }}" alt="{{ config('app.name') }} logo"
+    {{ $attributes->class('h-full w-full object-contain') }}>
